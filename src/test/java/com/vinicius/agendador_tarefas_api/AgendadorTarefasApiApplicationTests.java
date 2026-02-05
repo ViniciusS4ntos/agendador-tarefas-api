830058304848042b02af228a3994e60c57c3f521
ViniciusS4ntos <edsonklibrsouza2015@gmail.com>
2026-02-05 07:39:44 -0300

    commit inicial com esqueleto

---
+++ src/test/java/com/vinicius/agendador_tarefas_api/AgendadorTarefasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinicius.agendador_tarefas_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorTarefasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
